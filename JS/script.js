57173e034b257c1772138efdd01b46541a9d0421
Devansh Tomar <dktomar9456@gmail.com>
2021-07-02 18:32:54 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+var sno = 1;
+class entry {
+    constructor(sNo, upper, lower) {
+        this.sNo = sNo;
+        this.upper = upper;
+        this.lower = lower;
+    }
+}
+
+function saveIt() {
+
+    upper = document.getElementById('upper').value;
+    lower = document.getElementById('lower').value;
+    let nEntry = new entry(sno, upper, lower);
+    insertRaw(nEntry);
+    sno++;
+}
+
+function insertRaw(x) {
+    const list = document.getElementById('list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${x.sNo}</td>
+    <td style="background-color:${x.upper};">${x.upper}</td>
+    <td style="background-color:${x.lower};">${x.lower}</td> 
+    `
+    list.appendChild(row);
+}
