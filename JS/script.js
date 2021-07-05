7f01b523347528431b1e9c222a1e83b3825658a0
Devansh Tomar <dktomar9456@gmail.com>
2021-07-05 19:39:36 +0530

    added remove button and alerts

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,52 @@
-var sno = 1;
 class entry {
-    constructor(sNo, upper, lower) {
-        this.sNo = sNo;
+    constructor(upper, lower) {
         this.upper = upper;
         this.lower = lower;
     }
+}
+
+function removealert() {
+    var div = document.getElementById('alertdiv');
+    div.className = '';
+    div.innerHTML = '';
+}
+
+function showalert(message, classType) {
+    var div = document.getElementById('alertdiv');
+    div.className = "alert mx-5 alert-" + classType;
+    div.appendChild(document.createTextNode(message));
+    setTimeout(() => {
+        removealert();
+    }, 1500);
 }
 
 function saveIt() {
 
     upper = document.getElementById('upper').value;
     lower = document.getElementById('lower').value;
-    let nEntry = new entry(sno, upper, lower);
-    insertRaw(nEntry);
-    sno++;
+    if (upper == '#000000' && lower == '#000000') {
+        showalert("You Can't Choose both black. so choose different colour by clicking on the cloth shaped field ! :)", 'danger');
+    } else {
+        showalert("Style Saved Sucessfully", 'success');
+        let nEntry = new entry(upper, lower);
+        insertRaw(nEntry);
+    }
+
 }
 
 function insertRaw(x) {
     const list = document.getElementById('list');
     const row = document.createElement('tr');
     row.innerHTML = `
-    <td>${x.sNo}</td>
     <td style="background-color:${x.upper};">${x.upper}</td>
-    <td style="background-color:${x.lower};">${x.lower}</td> 
-    `
+    <td style="background-color:${x.lower};">${x.lower}</td>
+    <td><a href='#' class="btn btn-danger btn-sm" onclick="remove(this)">X</a></td>
+    `;
     list.appendChild(row);
 
+}
+
+function remove(element) {
+    element.parentElement.parentElement.remove();
+    showalert("Style Removed Sucessfully!", 'primary')
 }
